c6634a96cdad2df0d3956c93001537c9f65c70ef
IvanRavic88 <ravic.ivan88@gmail.com>
2023-12-21 12:57:09 +0100

    Implement authentication and user display in header components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,5 @@ import Profile from "@/components/profile";
 export default async function Home() {
   const session = await auth();
 
-  return (
-    <div>
-      <form action={action.signIn}>
-        <Button type="submit">Sign In</Button>
-      </form>
-
-      <form action={action.signOut}>
-        <Button type="submit">Sign Out</Button>
-      </form>
-      {session?.user ? (
-        <div>{JSON.stringify(session.user)}</div>
-      ) : (
-        <div>Signed Out</div>
-      )}
-      <Profile />
-    </div>
-  );
+  return <div></div>;
 }
